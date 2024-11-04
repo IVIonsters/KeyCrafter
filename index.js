93429a70d3982beea84b93be6d7e1750e9bdd14d
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2024-11-03 22:13:51 -0600

    Second Display
    added context to allow password to generate into both boxes.
    
    Need to modify function to generate a different password for reach display

--- index.js
+++ index.js
@@ -5,23 +5,25 @@ const numbers = '0123456789';
 const symbols = '!@#$%^&*()_+';
 const passwordLength = 14;
 
-//variable to combine all characters
+// Variable to combine all characters
 const addCharacters = upperCase + lowerCase + numbers + symbols;
 
+// DOM elements
+const passwordOne = document.getElementById('passwordOne');
 
-// generate password function
+// Generate password function
 function generatePassword() {
-    let password='';
-    //math.random to generate, math.floor to round down
+    let password = ''; // Reset password variable
+    // Math.random to generate, Math.floor to round down
     password += upperCase[Math.floor(Math.random() * upperCase.length)];
     password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
     password += numbers[Math.floor(Math.random() * numbers.length)];
     password += symbols[Math.floor(Math.random() * symbols.length)];
-  //for loop to generate password
+    // For loop to generate password
     for (let i = 0; i < passwordLength; i++) {
         password += addCharacters[Math.floor(Math.random() * addCharacters.length)];
-        console.log(password);
     }
+    passwordOne.textContent = password; // Update the DOM element
+    passwordTwo.textContent = password; // Update the DOM element
     return password;
-    
 }
