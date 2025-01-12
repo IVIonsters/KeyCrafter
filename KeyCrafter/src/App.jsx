936c2f3ef14ea56ea2617c85a9fcb889b6d2105f
Zachary Polof <46513785+IVIonsters@users.noreply.github.com>
2025-01-12 10:52:04 -0600

    add initial components and Tailwind CSS setup; implement Password Generator UI

--- KeyCrafter/src/App.jsx
+++ KeyCrafter/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
 
+
 function App() {
 
   return (
     <>
-      <h2>Hello, World</h2>
+      <body
+        className="bg-gradient-to-b from-gray-100 to-gray-300 min-h-screen flex items-center justify-center p-4"
+      >
+        <section
+          className="w-full max-w-md bg-white/40 backdrop-blur-lg p-6 rounded-xl shadow-2xl flex flex-col items-center text-center space-y-4 border border-gray-300/60"
+        >
+          <h1 className="text-3xl font-extrabold text-gray-900">Password Generator</h1>
+        </section>
+      </body >
     </>
   )
 }
